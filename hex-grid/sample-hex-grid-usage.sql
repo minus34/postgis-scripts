af0d97d1bbe2bbba8f016f4cfdef310f772d2c5b
Hugh Saalmans <hugh.saalmans@gmail.com>
2015-04-11 21:29:45 +1000

    Revert "improved hex-grid comments"
    This reverts commit a7c4f9c046fc0e78c07a9c7f6788fe90b59f800d.

--- hex-grid/sample-hex-grid-usage.sql
+++ hex-grid/sample-hex-grid-usage.sql
@@ -7,13 +7,13 @@
 --
 -- DESCRIPTION:
 -- 
--- Script creates a new table and populates it with a grid of ~18 million hexagons covering Australia using the hex grid function.
--- Takes under 10 mins on my 8 core, 16Gb RAM commodity PC to produce the hexagons, and another 10 mins to index & cluster.
+-- Script creates a new table and populates it with a grid of hexagons covering Australia using hex grid function.
+-- Takes under 10 mins on my 8 core, 16Gb RAM commodity PC to produce the hexagons, and another 10-15 mins to index & cluster
 --
--- Coordinate systems (SRIDs) used:
+-- Coordinate systems used:
 --   input:   GDA94 lat/long - SRID 4283
 --   working: Australian Albers Equal Area (GDA94) - SRID 3577
---   output:  GDA94 lat/long - SRID 4283
+--   output:  GDA94 lat/long
 --
 -- LICENSE
 --
@@ -29,19 +29,19 @@ CREATE TABLE my_hex_grid (
 )
 WITH (OIDS=FALSE);
 
--- Create 1km2 hex grid for Australia (note: extents allow for the effects of the Albers Equal Area projection)
--- Input parameters: hex_grid(areakm2 float, xmin float, ymin float, xmax float, ymax float, inputsrid integer, workingsrid integer, ouputsrid integer)
+-- Create 1km2 hex grid for Australia (extents are oversized to counter the effects of using the Albers Equal Area projection)
 INSERT INTO my_hex_grid (geom)
 select hex_grid(1.0, 108.0, -44.0, 151.0, -8.0, 4283, 3577, 4283);
+
+-- Update stats on table
+ANALYZE my_hex_grid;
 
 -- Create spatial index
 CREATE INDEX my_hex_grid_geom_idx ON my_hex_grid USING gist (geom);
 
--- Cluster table by spatial index (for spatial query performance)
+-- Cluster table by spatial index for performance
 CLUSTER my_hex_grid USING my_hex_grid_geom_idx;
 
--- Update stats on table
-ANALYZE my_hex_grid;
 
 --Check accuracy of results (in square km)
 SELECT (SELECT Count(*) From my_hex_grid) AS hexagon_count,

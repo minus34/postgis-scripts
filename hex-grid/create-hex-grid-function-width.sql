0b2eacb46d1ddaea16f06a75589833736f0a168d
Hugh Saalmans <hugh.saalmans@gmail.com>
2015-07-18 10:56:57 +1000

    Added create hex grid using width

---
+++ hex-grid/create-hex-grid-function-width.sql
@@ -0,0 +1,135 @@
+--------------------------------------------------------------------------------------------------------------------------------
+-- HEX GRID - Create function
+--------------------------------------------------------------------------------------------------------------------------------
+--
+-- Hugh Saalmans (@minus34)
+-- 2015/04/10
+--
+-- DESCRIPTION:
+-- 
+-- Function returns a grid of mathmatically correct hexagonal polygons.
+-- Useful for hexbinning (the art of mapping clusters of information unbiased by political/historical/statistical boundaries).
+--
+-- INPUT
+--
+--   areakm2     : area of each hexagon in square km.
+--                   - note: hexagon size can be off slightly due to coordinate rounding in the calcs.
+--
+--   xmin, ymin  : min coords of the grid extents.
+--
+--   xmax, ymax  : max coords of the grid extents.
+--
+--   inputsrid   : the coordinate system (SRID) of the input min/max coords.
+--
+--   workingsrid : the SRID used to process the polygons.
+--                   - SRID must be a projected coord sys (i.e. in metres) as the calcs require ints. Degrees are out.
+--                   - should be an equal area SRID such as Albers or Lambert Azimuthal (e.g. Australia = 3577, US = 2163).
+--                   - using Mercator will NOT return hexagons of equal area due to its distortions (don't try it in Greenland).
+--
+--   ouputsrid   : the SRID of the output polygons.
+--
+-- NOTES
+--
+--   Hexagon height & width are rounded up & down to the nearest metre, hence the area may be off slightly.
+--   This is due to the Postgres generate_series function which doesn't support floats.
+--
+--   Why are my areas wrong in QGIS, MapInfo, etc...?
+--      Let's assume you created WGS84 lat/long hexagons, you may have noticed the areas differ by almost half in a desktop GIS
+--      like QGIS or MapInfo Pro. This is due to the way those tools display geographic coordinate systems like WGS84 lat/long.
+--      Running the following query in PostGIS will confirm the min & max sizes of your hexagons (in km2):
+--
+--         SELECT (SELECT (MIN(ST_Area(geom::geography, FALSE)) / 1000000.0)::numeric(10,3) From my_hex_grid) AS minarea,
+--               (SELECT (MAX(ST_Area(geom::geography, FALSE)) / 1000000.0)::numeric(10,3) From my_hex_grid) AS maxarea;
+--
+--   Hey, why doesn't the grid cover the area I defined using my min/max extents?
+--      Assuming you used lat/long extents and processed the grid with an equal area projection, the projection caused your
+--      min/max coords to describe a conical shape, not a rectangular one - and the conical area didn't cover everything you
+--      wanted to include.  See au-hex-grid.png as an example of this.
+--      If you're bored - learn why projections distort maps here: http://www.icsm.gov.au/mapping/about_projections.html
+--
+--   This code is based on this PostGIS Wiki article: https://trac.osgeo.org/postgis/wiki/UsersWikiGenerateHexagonalGrid
+--
+--   Dimension calcs are based on formulae from: http://hexnet.org/content/hexagonal-geometry
+--
+-- LICENSE
+--
+-- This work is licensed under the Apache License, Version 2: https://www.apache.org/licenses/LICENSE-2.0
+--
+--------------------------------------------------------------------------------------------------------------------------------
+
+--CREATE EXTENSION postgis;
+
+
+-- DROP FUNCTION IF EXISTS hex_grid2_width(widthkm FLOAT, xmin FLOAT, ymin FLOAT, xmax FLOAT, ymax FLOAT, inputsrid INTEGER, workingsrid INTEGER, ouputsrid INTEGER, OUT gid TEXT, OUT geom geometry);
+CREATE OR REPLACE FUNCTION hex_grid_width(widthkm FLOAT, xmin FLOAT, ymin FLOAT, xmax FLOAT, ymax FLOAT, inputsrid INTEGER, workingsrid INTEGER, ouputsrid INTEGER, OUT gid TEXT, OUT hex geometry)
+  RETURNS SETOF record AS
+$BODY$
+
+DECLARE
+  minpnt GEOMETRY;
+  maxpnt GEOMETRY;
+  x1 INTEGER;
+  y1 INTEGER;
+  x2 INTEGER;
+  y2 INTEGER;
+  aream2 FLOAT;
+  qtrwidthfloat FLOAT;
+  qtrwidth INTEGER;
+  halfheight INTEGER;
+
+
+BEGIN
+
+  -- Convert input coords to points in the working SRID
+  minpnt = ST_Transform(ST_SetSRID(ST_MakePoint(xmin, ymin), inputsrid), workingsrid);
+  maxpnt = ST_Transform(ST_SetSRID(ST_MakePoint(xmax, ymax), inputsrid), workingsrid);
+
+  -- Get grid extents in working SRID coords
+  x1 = ST_X(minpnt)::INTEGER;
+  y1 = ST_Y(minpnt)::INTEGER;
+  x2 = ST_X(maxpnt)::INTEGER;
+  y2 = ST_Y(maxpnt)::INTEGER;
+
+  -- Get height and width parameters of hexagon (in metres) - FLOOR and CEILING are used to get the hexagon size closer to the requested input area
+  qtrwidthfloat := widthkm * 1000.0 / 4.0;
+  
+  qtrwidth := FLOOR(qtrwidthfloat);
+  halfheight := CEILING(qtrwidthfloat * sqrt(3.0));
+
+  -- Return the hexagons - done in pairs, with one offset from the other
+  RETURN QUERY (
+    SELECT DISTINCT sqt.* FROM (
+      SELECT pre || x_series::TEXT || y_series::TEXT AS gid, ST_Transform(ST_SetSRID(ST_Translate(geom, x_series::FLOAT, y_series::FLOAT), workingsrid), ouputsrid) AS hex
+        FROM generate_series(x1, x2, (qtrwidth * 6)) AS x_series,
+             generate_series(y1, y2, (halfheight * 2)) AS y_series,
+             (
+               SELECT '1' AS pre, ST_GeomFromText(
+                 format('POLYGON((0 0, %s %s, %s %s, %s %s, %s %s, %s %s, 0 0))',
+                   qtrwidth, halfheight,
+                   qtrwidth * 3, halfheight,
+                   qtrwidth * 4, 0,
+                   qtrwidth * 3, halfheight * -1,
+                   qtrwidth, halfheight * -1
+                 )
+               ) AS geom
+               UNION
+               SELECT '2' AS pre, ST_Translate(
+                 ST_GeomFromText(
+                   format('POLYGON((0 0, %s %s, %s %s, %s %s, %s %s, %s %s, 0 0))',
+                     qtrwidth, halfheight,
+                     qtrwidth * 3, halfheight,
+                     qtrwidth * 4, 0,
+                     qtrwidth * 3, halfheight * -1,
+                     qtrwidth, halfheight * -1
+                   )
+                 )
+               , qtrwidth * 3, halfheight) as geom
+             ) AS two_hex
+     ) AS sqt
+     INNER JOIN public.aus_buffer AS aus
+     ON ST_Intersects(aus.geom, sqt.hex)
+  );
+
+END$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
---
+++ hex-grid/create-hex-grid-using-width.py
@@ -0,0 +1,107 @@
+# Import Python modules
+import psycopg2
+
+from datetime import datetime
+
+#######################################################################################################################
+# SCRIPT PARAMETERS
+#######################################################################################################################
+
+# Postgres parameters
+pg_host = "localhost"
+pg_port = 5432
+pg_db = "iadpdev"
+pg_user = "postgres"
+pg_password = "password"
+pg_schema = "hex"
+
+hex_grid_table_name = "grid"
+
+# Grid parameters
+start_width = 1.0  # in km
+multiple = 2
+max_width = 1500
+
+# #####################################################################################################################
+
+startTime = datetime.now()
+
+print("--------------------------------------------------")
+print("PROCESSING STARTED")
+
+
+def main():
+    # Connect to Postgres
+    pg_connect_string = "host='%s' dbname='%s' user='%s' password='%s' port=%s"
+
+    # need to use password version as trust is not set on server
+    pg_conn = psycopg2.connect(pg_connect_string % (pg_host, pg_db, pg_user, pg_password, pg_port))
+
+    pg_conn.autocommit = True
+    pg_cur = pg_conn.cursor()
+
+    # Get Hex Grids
+    get_hex_grids(pg_cur)
+
+    pg_cur.close()
+    pg_conn.close()
+
+    return True
+
+
+def get_hex_grids(pg_cur):
+    curr_width = start_width
+
+    while curr_width < max_width:
+        # Get table name friendly width
+        curr_width_str = str(curr_width)
+        curr_width_str = curr_width_str.replace(".", "_")
+        curr_width_str = curr_width_str.replace("_0", "")
+
+        # Create hex grid table
+        pg_cur.execute("DROP TABLE IF EXISTS %s.%s_%s" % (pg_schema, hex_grid_table_name, curr_width_str))
+
+        sql = "CREATE UNLOGGED TABLE {0}.{1}_{2} (" \
+              "gid SERIAL, " \
+              "pid varchar(20), " \
+              "geom GEOMETRY('POLYGON', 4283, 2)" \
+              ") WITH (OIDS=FALSE)".format(pg_schema, hex_grid_table_name, curr_width_str)
+
+        pg_cur.execute(sql)
+
+        # Index the geom and cluster table on the index
+        sql = "CREATE INDEX %s_%s_geom_idx ON %s.%s_%s USING gist (geom)"
+        pg_cur.execute(sql % (hex_grid_table_name, curr_width_str, pg_schema, hex_grid_table_name, curr_width_str))
+
+        sql = "ALTER TABLE %s.%s_%s CLUSTER ON %s_%s_geom_idx"
+        pg_cur.execute(sql % (pg_schema, hex_grid_table_name, curr_width_str, hex_grid_table_name, curr_width_str))
+
+        # Generate hex grids
+        sql = "INSERT INTO {0}.{1}_{2} (pid, geom) " \
+              "SELECT * FROM hex_grid_width({2}, 84.0, -44.0, 161.5, -5.0, 4283, 3577, 4283)"\
+            .format(pg_schema, hex_grid_table_name, curr_width_str)
+
+        pg_cur.execute(sql)
+
+        pg_cur.execute("ANALYZE %s.%s_%s" % (pg_schema, hex_grid_table_name, curr_width_str))
+
+        print datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " - Grid %s processed" % curr_width_str
+
+        curr_width *= multiple
+
+    return True
+
+
+if __name__ == '__main__':
+    if main():
+        print("PROCESSING FINISHED")
+    else:
+        print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " - FATAL - PROCESSING STOPPED UNEXPECTEDLY")
+
+    duration = datetime.now() - startTime
+    days, seconds = duration.days, duration.seconds
+    hours = str(days * 24 + seconds // 3600).zfill(2)
+    minutes = str((seconds % 3600) // 60).zfill(2)
+    seconds = str(seconds % 60 + duration.microseconds // 1000000).zfill(2)
+
+    print("DURATION: " + hours + ":" + minutes + ":" + seconds)

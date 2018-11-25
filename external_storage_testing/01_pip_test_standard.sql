e1e3474fa39b39d91a6a37873bf3543ea5d6489a
Hugh Saalmans <hugh.saalmans@iag.com.au>
2018-11-25 18:21:00 +1100

    test results

--- external_storage_testing/01_pip_test_standard.sql
+++ external_storage_testing/01_pip_test_standard.sql
@@ -2,7 +2,11 @@
 -- run point in polygon test with standard geom field storage
 -- PG 10 Execute times 37 m 28 s
 -- PG 11 Execute times
-
+    -- 32 m 50 s 365 ms
+    -- 32 m 3 s 945 ms
+    -- 32 m 16 s 372 ms
+    -- 32 m 8 s 194 ms
+    -- 32 m 7 s 89 ms
 
 DROP TABLE IF EXISTS testing.pip_test_standard;
 CREATE TABLE testing.pip_test_standard AS

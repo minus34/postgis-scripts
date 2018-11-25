b9aee3d02af6c60ecf06e18b42928b6845a8bcb0
Hugh Saalmans <hugh.saalmans@iag.com.au>
2018-11-26 09:29:58 +1100

    test results

--- external_storage_testing/00_create_tables.sql
+++ external_storage_testing/00_create_tables.sql
@@ -1,11 +1,10 @@
 
 -- CREATE SCHEMA testing;
 
-
 -- create meshblocks table
 DROP TABLE if exists testing.abs_2016_mb;
 CREATE TABLE testing.abs_2016_mb (
---     gid serial NOT NULL,
+    --     gid serial NOT NULL,
     gid integer NOT NULL,
     mb_16code text NOT NULL,
     mb_category text,
@@ -32,9 +31,52 @@ ALTER TABLE testing.abs_2016_mb OWNER TO postgres;
 -- set geom column to decompressed
 ALTER TABLE testing.abs_2016_mb ALTER COLUMN geom SET STORAGE EXTERNAL;
 
+-- insert data -- 358,011 rows
+INSERT INTO testing.abs_2016_mb
+SELECT * FROM admin_bdys_201811.abs_2016_mb;
+
+ANALYZE testing.abs_2016_mb;
+
+-- add indexes and cluster on geom index
+ALTER TABLE testing.abs_2016_mb ADD CONSTRAINT abs_2016_mb_pk PRIMARY KEY (gid);
+
+CREATE INDEX abs_2016_mb_geom_idx ON testing.abs_2016_mb USING gist (geom);
+ALTER TABLE testing.abs_2016_mb CLUSTER ON abs_2016_mb_geom_idx;
+
+ANALYZE testing.abs_2016_mb;
+
+
+-- create subdivided meshblocks table
+DROP TABLE if exists testing.abs_2016_mb_subd;
+CREATE TABLE testing.abs_2016_mb_subd (
+    gid serial NOT NULL,
+    mb_16code text NOT NULL,
+    mb_category text,
+    sa1_16main double precision,
+    sa1_16_7cd integer,
+    sa2_16main integer,
+    sa2_16_5cd integer,
+    sa2_16name text,
+    sa3_16code integer,
+    sa3_16name text,
+    sa4_16code integer,
+    sa4_16name text,
+    gcc_16code text,
+    gcc_16name text,
+    state text,
+    area_sqm numeric,
+    mb16_pop integer,
+    mb16_dwell integer,
+    geom geometry(Polygon,4283) NULL
+)
+WITH (OIDS = FALSE);
+ALTER TABLE testing.abs_2016_mb_subd OWNER TO postgres;
+
+-- set geom column to decompressed
+ALTER TABLE testing.abs_2016_mb_subd ALTER COLUMN geom SET STORAGE EXTERNAL;
+
 -- insert data -- 385,263 rows
-INSERT INTO testing.abs_2016_mb (
-    gid,
+INSERT INTO testing.abs_2016_mb_subd (
     mb_16code,
     mb_category,
     sa1_16main,
@@ -53,8 +95,7 @@ INSERT INTO testing.abs_2016_mb (
     mb16_pop,
     mb16_dwell,
     geom)
-SELECT gid,
-       mb_16code,
+SELECT mb_16code,
        mb_category,
        sa1_16main,
        sa1_16_7cd,
@@ -71,19 +112,18 @@ SELECT gid,
        area_sqm,
        mb16_pop,
        mb16_dwell,
-       geom
---        ST_Subdivide(geom, 512)
+       ST_Subdivide(geom, 512)
 FROM admin_bdys_201811.abs_2016_mb;
 
-ANALYZE testing.abs_2016_mb;
+ANALYZE testing.abs_2016_mb_subd;
 
 -- add indexes and cluster on geom index
-ALTER TABLE testing.abs_2016_mb ADD CONSTRAINT abs_2016_mb_pk PRIMARY KEY (gid);
+ALTER TABLE testing.abs_2016_mb_subd ADD CONSTRAINT abs_2016_mb_subd_pk PRIMARY KEY (gid);
 
-CREATE INDEX abs_2016_mb_geom_idx ON testing.abs_2016_mb USING gist (geom);
-ALTER TABLE testing.abs_2016_mb CLUSTER ON abs_2016_mb_geom_idx;
+CREATE INDEX abs_2016_mb_subd_geom_idx ON testing.abs_2016_mb_subd USING gist (geom);
+ALTER TABLE testing.abs_2016_mb_subd CLUSTER ON abs_2016_mb_subd_geom_idx;
 
-ANALYZE testing.abs_2016_mb;
+ANALYZE testing.abs_2016_mb_subd;
 
 
 -- create GNAF table -- 13,810,270
--- external_storage_testing/02_pip_test_external.sql
+++ external_storage_testing/02_pip_test_external.sql
@@ -8,19 +8,19 @@
     -- 32 m 26 s 238 ms
     -- 32 m 27 s 546 ms
 
-DROP TABLE IF EXISTS testing.pip_test_external;
-CREATE TABLE testing.pip_test_external AS
-  SELECT gnaf.gnaf_pid,
-         mb.mb_16code
-  FROM testing.abs_2016_mb AS mb
-  inner join testing.address_principals as gnaf
-  on st_intersects(gnaf.geom, mb.geom);
+-- PG 11 Execute times - ST_Subdivide
+-- 32 m 2 s 126 ms
+-- 30 m 42 s 740 ms
+-- 30 m 48 s 532 ms
+-- 30 m 41 s 439 ms
+-- 30 m 43 s 38 ms
+
 
 DROP TABLE IF EXISTS testing.pip_test_external;
 CREATE TABLE testing.pip_test_external AS
   SELECT gnaf.gnaf_pid,
          mb.mb_16code
-  FROM testing.abs_2016_mb AS mb
+  FROM testing.abs_2016_mb_subd AS mb
          inner join testing.address_principals as gnaf
            on st_intersects(gnaf.geom, mb.geom);
 
@@ -28,7 +28,7 @@ DROP TABLE IF EXISTS testing.pip_test_external;
 CREATE TABLE testing.pip_test_external AS
   SELECT gnaf.gnaf_pid,
          mb.mb_16code
-  FROM testing.abs_2016_mb AS mb
+  FROM testing.abs_2016_mb_subd AS mb
          inner join testing.address_principals as gnaf
            on st_intersects(gnaf.geom, mb.geom);
 
@@ -36,7 +36,7 @@ DROP TABLE IF EXISTS testing.pip_test_external;
 CREATE TABLE testing.pip_test_external AS
   SELECT gnaf.gnaf_pid,
          mb.mb_16code
-  FROM testing.abs_2016_mb AS mb
+  FROM testing.abs_2016_mb_subd AS mb
          inner join testing.address_principals as gnaf
            on st_intersects(gnaf.geom, mb.geom);
 
@@ -44,6 +44,14 @@ DROP TABLE IF EXISTS testing.pip_test_external;
 CREATE TABLE testing.pip_test_external AS
   SELECT gnaf.gnaf_pid,
          mb.mb_16code
-  FROM testing.abs_2016_mb AS mb
+  FROM testing.abs_2016_mb_subd AS mb
+         inner join testing.address_principals as gnaf
+           on st_intersects(gnaf.geom, mb.geom);
+
+DROP TABLE IF EXISTS testing.pip_test_external;
+CREATE TABLE testing.pip_test_external AS
+  SELECT gnaf.gnaf_pid,
+         mb.mb_16code
+  FROM testing.abs_2016_mb_subd AS mb
          inner join testing.address_principals as gnaf
            on st_intersects(gnaf.geom, mb.geom);
